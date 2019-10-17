cb293b213fc54a5927a857cd2ed9074cb1e234af
Brais Vidal <brais.vidal.rodriguez@gmail.com>
2019-10-17 21:38:34 +0200

    heroku

---
+++ server/public/precache-manifest.dd7b3f5af9a534b155cb70899feed2fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d88e32c713e57e87ce2",
+    "url": "/static/js/main.3d88e32c.chunk.js"
+  },
+  {
+    "revision": "89e6f8a61a7f7e07f963",
+    "url": "/static/js/1.89e6f8a6.chunk.js"
+  },
+  {
+    "revision": "3d88e32c713e57e87ce2",
+    "url": "/static/css/main.aea256e2.chunk.css"
+  },
+  {
+    "revision": "6a842ccabb958f70ab448314dda825ea",
+    "url": "/index.html"
+  }
+];
